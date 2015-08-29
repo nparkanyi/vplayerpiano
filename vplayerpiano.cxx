eff7fbf3dc45cbee85c02b56cdf602230ad97a00
Nicholas Parkanyi <n.parkanyi@gmail.com>
2015-08-29 00:36:51 -0400

    Keyboard class

--- vplayerpiano.cxx
+++ vplayerpiano.cxx
@@ -14,3 +14,61 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include <SDL.h>
+#include <iostream>
+
+
+class Keyboard
+{
+public:
+  Keyboard(SDL_Surface * out);
+
+private:
+  void genKeyRects();
+
+  int key_width = 9; //width (including gap) of a white key in pixels
+  int key_height; //determined relative to the width
+  bool key_states[88];
+  SDL_Rect key_rects[88];
+};
+
+
+Keyboard::Keyboard(SDL_Surface * out) : key_height(6 * key_width){
+  for (bool &key : key_states){
+    key = false;
+  }
+
+  genKeyRects();
+}
+
+void Keyboard::genKeyRects(){
+  int offset = 0;
+  int black_width = 2 / 3 * key_width;
+  int black_height = 2 / 3 * key_height;
+
+  for (int i = 0; i < 88; i++){
+    if (i % 12 == 1 || i % 12 == 6){ //Bb and Eb
+      key_rects[i].h = black_height;
+      key_rects[i].w = black_width;
+      key_rects[i].x = offset - key_width / 6;
+      key_rects[i].y = 0;
+    } else if (i % 12 == 4 || i % 12 == 9){ //Db and Gb
+      key_rects[i].h = black_height;
+      key_rects[i].w = black_width;
+      key_rects[i].x = offset - 1 / 2 * key_width;
+      key_rects[i].y = 0;
+    } else if (i % 12 == 11){ //Ab
+      key_rects[i].h = black_height;
+      key_rects[i].w = black_width;
+      key_rects[i].x = offset - 1 / 3 * key_width;
+      key_rects[i].y = 0;
+    } else { //white notes
+      key_rects[i].h = key_height;
+      key_rects[i].w = key_width - 2;
+      key_rects[i].x = offset + 1;
+      key_rects[i].y = 0;
+      offset += key_width;
+    }
+  }
+}
